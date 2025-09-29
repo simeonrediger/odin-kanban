ecc422c4218b08654afe8604477fcc543939a2f1
Simeon Rediger <simeonrediger@gmail.com>
2025-09-29 18:36:50 -0400

    Create method to add list

--- src/app/project.js
+++ src/app/project.js
@@ -1,7 +1,29 @@
+import List from './list.js';
+
 export default class Project {
 
     constructor(name, lists) {
         this.name = name;
         this.lists = lists;
+    }
+
+    addList(list) {
+        const isNotList = !(list instanceof List);
+        const listNameAlreadyExists = this.lists.some(
+            existingList => list.name === existingList.name
+        );
+
+        if (isNotList || listNameAlreadyExists) {
+
+            if (isNotList) {
+                throw new TypeError(`Not an instance of ${List.name}`);
+            }
+
+            if (listNameAlreadyExists) {
+                throw new DuplicateNameError(list.name);
+            }
+        }
+
+        this.lists.push(list);
     }
 }
