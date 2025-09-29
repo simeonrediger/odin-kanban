193599bd5697a6ce4f81144b73bc3bb2a87b9ebb
Simeon Rediger <simeonrediger@gmail.com>
2025-09-29 18:06:25 -0400

    Refactor to use module scope instead of IIFE

--- src/app/workspace.js
+++ src/app/workspace.js
@@ -1,47 +1,47 @@
 import Project from './project.js';
 import DuplicateNameError from '../errors/duplicate-name-error.js';
 
-export default (function workspace() {
-    const projects = [];
-    let activeProject;
+const projects = [];
+let activeProject;
 
-    function addProject(project) {
-        const isNotProject = !(project instanceof Project);
-        const projectNameAlreadyExists = projects.some(
-            existingProject => project.name === existingProject.name
-        );
+function addProject(project) {
+    const isNotProject = !(project instanceof Project);
+    const projectNameAlreadyExists = projects.some(
+        existingProject => project.name === existingProject.name
+    );
 
-        if (isNotProject || projectNameAlreadyExists) {
+    if (isNotProject || projectNameAlreadyExists) {
 
-            if (isNotProject) {
-                throw new ValueError(
-                    "Project must be an instance of 'Project'"
-                );
-            }
-
-            if (projectNameAlreadyExists) {
-                throw new DuplicateNameError(project.name);
-            }
+        if (isNotProject) {
+            throw new ValueError(
+                "Project must be an instance of 'Project'"
+            );
         }
 
-        projects.push(project);
-    }
-
-    function getActiveProject() {
-
-        if (activeProject) {
-            return activeProject;
-
-        } else if (projects.length >= 1) {
-            return projects[0];
-
-        } else {
-            return null;
+        if (projectNameAlreadyExists) {
+            throw new DuplicateNameError(project.name);
         }
     }
 
-    return {
-        addProject,
-        getActiveProject,
-    };
-})();
+    projects.push(project);
+}
+
+function getActiveProject() {
+
+    if (activeProject) {
+        return activeProject;
+
+    } else if (projects.length >= 1) {
+        return projects[0];
+
+    } else {
+        return null;
+    }
+}
+
+const workspace = {
+    addProject,
+    getActiveProject,
+};
+
+export default workspace;
