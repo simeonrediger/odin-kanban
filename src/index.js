f8e45e5960f427066ad878835266b0fb42ea50a9
Simeon Rediger <simeonrediger@gmail.com>
2025-09-29 12:45:36 -0400

    Add default project stub

---
+++ src/index.js
@@ -0,0 +1,47 @@
+const defaultProject = {
+    name: 'Project Name',
+    lists: [
+        {
+            name: 'List A',
+            tasks: [
+                {
+                    name: 'Task A1',
+                },
+                {
+                    name: 'Task A2',
+                },
+            ],
+        },
+        {
+            name: 'List B',
+            tasks: [
+                {
+                    name: 'Task B1',
+                },
+                {
+                    name: 'Task B2',
+                },
+                {
+                    name: 'Task B3',
+                },
+                {
+                    name: 'Task B4',
+                },
+            ],
+        },
+        {
+            name: 'List C',
+            tasks: [
+                {
+                    name: 'Task C1',
+                },
+                {
+                    name: 'Task C2',
+                },
+                {
+                    name: 'Task C3',
+                },
+            ],
+        },
+    ],
+};
