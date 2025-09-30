80da3a53d6cb83fff983a361a1bf695fd122ff86
Simeon Rediger <simeonrediger@gmail.com>
2025-09-30 09:54:27 -0400

    Remove demo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
 import workspace from './models/workspace.js';
 import defaultBoard from './data/default-board.js';
 
-//Demo
-import Project from './app/project.js';
-import List from './app/list.js';
-import Task from './app/task.js';
-
 workspace.addBoard(defaultBoard);
-
-// Demo
-defaultProject.addList(new List('List D'));
-console.log(JSON.stringify(workspace.getActiveProject(), null, 2));
