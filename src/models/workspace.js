2a192875f76179dbe2a0d4da2b273ac677d9f608
Simeon Rediger <simeonrediger@gmail.com>
2025-10-01 12:17:34 -0400

    Convert active board getter to accessor

--- src/models/workspace.js
+++ src/models/workspace.js
@@ -9,24 +9,26 @@ function removeBoard(targetBoard) {
     boards.splice(boards.indexOf(targetBoard), 1);
 }
 
-function getActiveBoard() {
-
-    if (activeBoard) {
-        return activeBoard;
-
-    } else if (boards.length >= 1) {
-        return boards[0];
-
-    } else {
-        return null;
-    }
-}
-
 const workspace = {
     boards,
     addBoard,
     removeBoard,
-    getActiveBoard,
+
+    get activeBoard() {
+        if (activeBoard) {
+            return activeBoard;
+
+        } else if (boards.length >= 1) {
+            return boards[0];
+
+        } else {
+            return null;
+        }
+    },
+
+    set activeBoard(board) {
+        activeBoard = board;
+    }
 };
 
 export default workspace;
