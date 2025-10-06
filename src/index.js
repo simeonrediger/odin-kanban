e974fa225f95f806bf0edc9996526e37d807b7d7
Simeon Rediger <simeonrediger@gmail.com>
2025-10-06 19:42:07 -0400

    Rearrange import statements

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import '@/shared/styles/reset.css';
 import '@/shared/styles/colors.css';
 
-import workspace from '@/domains/workspace/model/workspace.js';
 import defaultBoard from '@/domains/board/defaults/board.js';
+import workspace from '@/domains/workspace/model/workspace.js';
 
 workspace.addBoard(defaultBoard);
