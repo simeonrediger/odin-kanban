5893d9ab258b282d6187c16448f9d0e892fd0cda
Simeon Rediger <simeonrediger@gmail.com>
2025-10-07 10:06:30 -0400

    Introduce workspace view

---
+++ src/domains/workspace/components/workspace-view.js
@@ -0,0 +1,4 @@
+const workspaceView = {
+};
+
+export default workspaceView;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import '@/shared/styles/colors.css';
 
 import defaultBoard from '@/domains/board/defaults/board.js';
 import workspace from '@/domains/workspace/model/workspace.js';
+import workspaceView from '@/domains/workspace/components/workspace-view.js';
 
 workspace.addBoard(defaultBoard);
