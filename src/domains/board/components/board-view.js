81e9b487515e092069fd63d26ff395c6d50d9249
Simeon Rediger <simeonrediger@gmail.com>
2025-10-08 22:24:51 -0400

    Validate board entity and container

--- src/domains/board/components/board-view.js
+++ src/domains/board/components/board-view.js
@@ -1,11 +1,17 @@
 import '../styles/board.css';
 
+import assert from '@/shared/validation/assert.js';
+import Board from '../model/board.js';
+
 let board;
 let container;
 
 function render(boardEntity, containerElement) {
     board = boardEntity;
     container = containerElement;
+
+    assert.instanceOf(Board, board, "'board'");
+    assert.instanceOf(Element, container, "'container'");
 }
 
 const boardView = {
