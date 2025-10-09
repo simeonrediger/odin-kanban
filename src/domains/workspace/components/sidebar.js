eb43086334d8ed733ab0dc6f87be6fcc92c328e2
Simeon Rediger <simeonrediger@gmail.com>
2025-10-08 23:34:02 -0400

    Extract sidebar view into its own module

---
+++ src/domains/workspace/components/sidebar.js
@@ -0,0 +1,84 @@
+import '@/shared/styles/utilities.css';
+import '@/shared/styles/icon-button.css';
+import '../styles/sidebar.css'; 
+
+import assert from '@/shared/validation/assert.js';
+
+let workspace;
+let container;
+let createNewBoardButton;
+let boardList;
+
+function render(workspaceEntity, containerElement) {
+    workspace = workspaceEntity;
+    container = containerElement;
+    assert.notNull(container, "'container'");
+
+    setUpElementReferences();
+    renderBoardList();
+}
+
+function setUpElementReferences() {
+    createNewBoardButton = container.querySelector(
+        "[data-action='create-new-board']"
+    );
+    boardList = container.querySelector('.board-list');
+
+    assert.notNull(createNewBoardButton, "'createNewBoardButton'");
+    assert.notNull(boardList, "'boardList'");
+}
+
+function renderBoardList() {
+    boardList.innerHTML = '';
+
+    for (const board of workspace.boards) {
+        const boardListItem = createBoardListItem(board);
+        boardList.append(boardListItem);
+    }
+}
+
+function createBoardListItem(board) {
+    const boardListItem = document.createElement('li');
+    boardListItem.classList.add('board-list-item');
+
+    const boardSelectButton = createBoardSelectButton(board);
+    const boardOptionsButton = createBoardOptionsButton();
+    boardListItem.append(boardSelectButton, boardOptionsButton);
+
+    return boardListItem;
+}
+
+function createBoardSelectButton(board) {
+    const boardSelectButton = document.createElement('button');
+    boardSelectButton.classList.add('board-select-button');
+    boardSelectButton.textContent = board.name;
+
+    return boardSelectButton;
+}
+
+function createBoardOptionsButton() {
+    const boardOptionsButton = document.createElement('button');
+    boardOptionsButton.classList.add('board-options-button');
+    boardOptionsButton.title = 'Open board options menu';
+    boardOptionsButton.ariaLabel = 'Open board options menu';
+
+    const boardOptionsIcon = createBoardOptionsIcon();
+    boardOptionsButton.append(boardOptionsIcon);
+
+    return boardOptionsButton;
+}
+
+function createBoardOptionsIcon() {
+    const boardOptionsIcon = container
+        .querySelector("[data-template='three-dots-horizontal-icon']")
+        .content
+        .cloneNode(true);
+
+    return boardOptionsIcon;
+}
+
+const sidebar = {
+    render,
+};
+
+export default sidebar;
--- src/domains/workspace/components/workspace-view.js
+++ src/domains/workspace/components/workspace-view.js
@@ -1,15 +1,12 @@
-import '@/shared/styles/utilities.css';
-import '@/shared/styles/icon-button.css';
 import '../styles/workspace.css';
-import '../styles/sidebar.css';
 
 import assert from '@/shared/validation/assert.js';
 import boardView from '@/domains/board/components/board-view.js';
+import sidebar from './sidebar.js';
 
 let workspace;
 let container;
-let createNewBoardButton;
-let boardList;
+let sidebarContainer;
 let boardContainer;
 let boardPlaceholder;
 
@@ -21,7 +18,7 @@ function render(workspaceEntity, containerElement) {
     assert.instanceOf(Element, container, "'container'");
 
     setUpElementReferences();
-    renderBoardList();
+    sidebar.render(workspace, sidebarContainer);
 
     if (workspace.activeBoard) {
         boardPlaceholder.classList.add('hidden');
@@ -33,71 +30,17 @@ function render(workspaceEntity, containerElement) {
 }
 
 function setUpElementReferences() {
-    createNewBoardButton = container.querySelector(
-        "[data-action='create-new-board']"
-    );
-    boardList = container.querySelector('.board-list');
+    sidebarContainer = container.querySelector('.sidebar');
     boardContainer = container.querySelector('.board-container');
     boardPlaceholder = container.querySelector('.board-placeholder');
 
-    assert.notNull(createNewBoardButton, "'createNewBoardButton'");
-    assert.notNull(boardList, "'boardList'");
     assert.notNull(boardContainer, "'boardContainer'");
     assert.notNull(boardPlaceholder, "'boardPlaceholder'");
-}
-
-function renderBoardList() {
-    boardList.innerHTML = '';
-
-    for (const board of workspace.boards) {
-        const boardListItem = createBoardListItem(board);
-        boardList.append(boardListItem);
-    }
 }
 
 function renderBoardPlaceholder() {
     boardContainer.classList.add('hidden');
     boardPlaceholder.classList.remove('hidden');
-}
-
-function createBoardListItem(board) {
-    const boardListItem = document.createElement('li');
-    boardListItem.classList.add('board-list-item');
-
-    const boardSelectButton = createBoardSelectButton(board);
-    const boardOptionsButton = createBoardOptionsButton();
-    boardListItem.append(boardSelectButton, boardOptionsButton);
-
-    return boardListItem;
-}
-
-function createBoardSelectButton(board) {
-    const boardSelectButton = document.createElement('button');
-    boardSelectButton.classList.add('board-select-button');
-    boardSelectButton.textContent = board.name;
-
-    return boardSelectButton;
-}
-
-function createBoardOptionsButton() {
-    const boardOptionsButton = document.createElement('button');
-    boardOptionsButton.classList.add('board-options-button');
-    boardOptionsButton.title = 'Open board options menu';
-    boardOptionsButton.ariaLabel = 'Open board options menu';
-
-    const boardOptionsIcon = createBoardOptionsIcon();
-    boardOptionsButton.append(boardOptionsIcon);
-
-    return boardOptionsButton;
-}
-
-function createBoardOptionsIcon() {
-    const boardOptionsIcon = container
-        .querySelector("[data-template='three-dots-horizontal-icon']")
-        .content
-        .cloneNode(true);
-
-    return boardOptionsIcon;
 }
 
 const workspaceView = {
