87567a3f6b9656638a52f3aa14d238a8a218f7c9
Simeon Rediger <simeonrediger@gmail.com>
2025-10-13 07:11:40 -0400

    Remove consumer-side validation

--- src/domains/board/components/board-view.js
+++ src/domains/board/components/board-view.js
@@ -1,17 +1,11 @@
 import '../styles/board.css';
 
-import assert from '@/shared/validation/assert.js';
-import Board from '../model/board.js';
-
 let board;
 let container;
 
 function render(boardEntity, containerElement) {
     board = boardEntity;
     container = containerElement;
-
-    assert.instanceOf(Board, board, "'board'");
-    assert.instanceOf(Element, container, "'container'");
 }
 
 const boardView = {
