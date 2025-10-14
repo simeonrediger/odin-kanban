708f57f4a45b5502d7014222657d09e4dc2ce0a2
Simeon Rediger <simeonrediger@gmail.com>
2025-10-14 11:43:20 -0400

    Create references for and validate static elements

--- src/domains/board/component.js
+++ src/domains/board/component.js
@@ -6,6 +6,8 @@ import ListView from '@/domains/list/component.js';
 let board;
 let container;
 let boardTitle;
+let createNewListButton;
+let listsContainer;
 
 function render(boardEntity, containerElement) {
     board = boardEntity;
@@ -20,14 +22,20 @@ function render(boardEntity, containerElement) {
 
     for (const list of board.lists) {
         const listView = new ListView(list);
-        container.querySelector("[data-role='lists-container']")
-            .append(listView.container);
+        listsContainer.append(listView.container);
     }
 }
 
 function setUpElementReferences() {
     boardTitle = container.querySelector("[data-role='board-title']");
+    createNewListButton = container.querySelector(
+        "[data-action='create-new-list']"
+    );
+    listsContainer = container.querySelector("[data-role='lists-container']");
+
     assert.notNull(boardTitle, "'boardTitle'");
+    assert.notNull(createNewListButton, "'createNewListButton'");
+    assert.notNull(listsContainer, "'listsContainer'");
 }
 
 function removeAllListViews() {
