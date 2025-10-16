106c338e77cae35fa9a12374dee8277b0b1766a2
Simeon Rediger <simeonrediger@gmail.com>
2025-10-16 18:16:40 -0400

    Set up element references

--- src/domains/workspace/components/board-editor.js
+++ src/domains/workspace/components/board-editor.js
@@ -1,7 +1,18 @@
 let container;
+let nameInput;
+let cancelButton;
+let confirmButton;
 
 function init(containerElement) {
     container = containerElement;
+
+    nameInput = container.querySelector("[data-input='board-name']");
+    cancelButton = container.querySelector(
+        "[data-action='cancel-board-editor']"
+    );
+    confirmButton = container.querySelector(
+        "[data-action='confirm-board-editor']"
+    );
 }
 
 const boardEditor = {
