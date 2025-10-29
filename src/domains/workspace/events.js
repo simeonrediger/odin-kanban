271d6a4b0c5d7c340b50cdaf2cac142e3c31faf7
Simeon Rediger <simeonrediger@gmail.com>
2025-10-28 21:12:00 -0400

    Add board deletion intent and notification events

--- src/domains/workspace/events.js
+++ src/domains/workspace/events.js
@@ -33,6 +33,16 @@
  * @property {string} boardName - New name of the board.
  */
 
+/**
+ * @typedef {Object} BoardDeletionRequestedDetail
+ * @property {string} boardId - Unique ID of the board.
+ */
+
+/**
+ * @typedef {Object} BoardDeletedDetail
+ * @property {string} boardId - Unique ID of the board.
+ */
+
 
 
 // Event names
@@ -84,3 +94,19 @@ export const BOARD_NAME_UPDATE_REQUESTED = 'board:name-update-requested';
  * @see {@link BoardNameUpdatedDetail}
  */
 export const BOARD_NAME_UPDATED = 'board:name-updated';
+
+/**
+ * @description Emitted when board deletion is requested.
+ * @event board:deletion-requested
+ * @type {string}
+ * @see {@link BoardDeletionRequestedDetail}
+ */
+export const BOARD_DELETION_REQUESTED = 'board:deletion-requested';
+
+/**
+ * @description Emitted when a board name is deleted.
+ * @event board:deleted
+ * @type {string}
+ * @see {@link BoardDeletedDetail}
+ */
+export const BOARD_DELETED = 'board:deleted';
