069e5f02bb738c919b0b89cc710486976765c1e2
Simeon Rediger <simeonrediger@gmail.com>
2025-11-02 14:43:56 -0500

    Privatize members not needed externally

--- src/domains/task/component.js
+++ src/domains/task/component.js
@@ -1,17 +1,23 @@
 import './style.css';
 
 export default class TaskView {
+    #container;
+    #label;
 
     constructor(taskId, store, containerRole) {
-        this.container = document.createElement('li');
-        this.container.dataset.id = taskId;
-        this.container.dataset.role = containerRole;
-        this.container.classList.add('task-container');
+        this.#container = document.createElement('li');
+        this.#container.dataset.id = taskId;
+        this.#container.dataset.role = containerRole;
+        this.#container.classList.add('task-container');
 
-        this.label = document.createElement('p');
-        this.label.textContent = store.getTaskName(taskId);
-        this.label.classList.add('task-label');
+        this.#label = document.createElement('p');
+        this.#label.textContent = store.getTaskName(taskId);
+        this.#label.classList.add('task-label');
 
-        this.container.append(this.label);
+        this.#container.append(this.#label);
+    }
+
+    get container() {
+        return this.#container;
     }
 }
