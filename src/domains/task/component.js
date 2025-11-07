5423caad0318427ac986ee9ac9ddd82d7f64fbb1
Simeon Rediger <simeonrediger@gmail.com>
2025-11-07 17:46:10 -0500

    Move id, store, and containerRole into init()
    Moves id, store, and containerRole use and initialization into init(),
    thereby allowing for TaskView instantiation without model data.

--- src/domains/task/component.js
+++ src/domains/task/component.js
@@ -6,15 +6,23 @@ export default class TaskView {
 
     constructor(taskId, store, containerRole) {
         this.#container = document.createElement('li');
-        this.#container.dataset.id = taskId;
-        this.#container.dataset.role = containerRole;
         this.#container.classList.add('task-container');
 
         this.#label = document.createElement('p');
-        this.#label.textContent = store.getTaskName(taskId);
         this.#label.classList.add('task-label');
 
         this.#container.append(this.#label);
+
+        if (taskId) {
+            this.init(taskId, store, containerRole);
+        }
+    }
+
+    init(taskId, store, containerRole) {
+        this.#container.dataset.id = taskId;
+        this.#container.dataset.role = containerRole;
+
+        this.#label.textContent = store.getTaskName(taskId);
     }
 
     get container() {
